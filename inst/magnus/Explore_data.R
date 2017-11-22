3885c5e94c0a306c30ed57659f21d1e0ee94c642
MagnusBitsch <magnusbitsch@gmail.com>
2017-11-22 21:28:46 +0100

    magnus explore data

---
+++ inst/magnus/Explore_data.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+options(scipen = 99999999)
+
+source('./inst/magnus/load_data.R')
+
+str(data_list[["train"]])
+summary(data_list[["train"]])
+
+diff_time <- difftime(Sys.time(), data_list[["train"]]$date)
+
+hist(data_list[["train"]]$id, col = 8)
+plot(data_list[["train"]]$date[sample(1:length(data_list[["train"]]$date),10000)])
+hist(as.numeric(diff_time), col = 8)
+hist(data_list[["train"]]$store_nbr, col = 8)
+
+write.table(1:10, './.')
